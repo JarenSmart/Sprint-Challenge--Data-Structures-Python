86500977dc1dfb21f0b185212a987f5e26b5dbb3
jaren <jarensmart@yahoo.com>
2020-08-30 19:42:18 -0400

    Finished reverse.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,23 @@
+# buffer = RingBuffer(3)
+
+# buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# buffer.get()   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# buffer.get()   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get()   # should return ['d', 'e', 'f']
+
 class RingBuffer:
     def __init__(self, capacity):
         pass
